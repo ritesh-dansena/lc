ddad1e6343ec19482d104d40534513a88584d699
ritesh-dansena <rdworks18@gmail.com>
2026-09-25 20:21:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (36.00%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        long long original = x ;
+        long long reverse = 0;
+        while(x>0){
+            int digit = x%10;
+            reverse  = reverse*10 +digit;
+            x/=10;}
+            if( original == reverse){
+                return true;
+            }
+            return false;
+    }
+};  
